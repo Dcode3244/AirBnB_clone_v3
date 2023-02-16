b72bfd38351ad177525ba894312fd02e7240971d
dcode3244 <dagimourgie111@gmail.com>
2023-02-16 17:44:03 +0000

    advanced task 13

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,61 +1,64 @@
 #!/usr/bin/python3
-""" return dict repersantation of object """
+"""
+handles all default RESTFul API actions:
+"""
+
 from models import storage, storage_t
-from models.engine.db_storage import classes
 from api.v1.views import app_views
-from flask import jsonify, abort, request, make_response
+from flask import jsonify, request, make_response, abort
+from models.engine.db_storage import classes
 
 
-@app_views.route("/places/<place_id>/amenities",
-                 methods=["GET"], strict_slashes=False)
-def PA_get(place_id):
-    result = []
-    """ get all the reviews in a place """
-    task = storage.get(classes['Place'], place_id)
-    if not task:
-        abort(404)
-    for i in task.amenities:
-        result.append(i.to_dict())
-    return jsonify(result)
+@app_views.route('/places/<place_id>/amenities', methods=["GET"])
+def get_amenities_by_place(place_id):
+    """ defines route to api/v1/places/<place_id>/amenities """
+    if request.method == 'GET':
+        place = storage.get(classes['Place'], place_id)
+        if not place:
+            abort(404)
+        amenity = [a.to_dict() for a in place.amenities]
+        return jsonify(amenity)
 
 
-@app_views.route("/places/<place_id>/amenities/<amenity_id>",
-                 methods=['DELETE'], strict_slashes=False)
-def PA_specific_delete(place_id, amenity_id):
-    """ delete the inputed object from review """
-    place = storage.get(classes['Place'], place_id)
-    task = storage.get(classes['Amenity'], amenity_id)
-    if not place or not task:
-        abort(404)
-    if storage_t == 'db':
-        check = place.amenities
-        tasks = task
-    else:
-        check = place.amenity_id
-        tasks = task.id
-    for i in range(len(check)):
-        if check[i] == tasks:
-            check.pop(i)
-            storage.save()
-            return jsonify({}), 200
-    abort(404)
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=["DELETE", "POST"])
+def handle_amenities_by_id(place_id, amenity_id):
+    """ defines route to api/v1/places/<place_id>/amenities/<amenity_id> """
+    if request.method == 'DELETE':
+        place = storage.get(classes['Place'], place_id)
+        if not place:
+            abort(404)
+        amenity = storage.get(classes['Amenity'], amenity_id)
+        if (not amenity):
+            abort(404)
+        if storage_t == "db":
+            if amenity not in place.amenities:
+                abort(404)
+            place.amenities.remove(amenity)
+        else:
+            if amenity.id not in place.amenity_id:
+                abort(404)
+            place.amenity_id.remove(amenity.id)
 
+        storage.save()
+        return jsonify({}), 200
 
-@app_views.route("/places/<place_id>/amenities/<amenity_id>",
-                 methods=['POST'], strict_slashes=False)
-def PA_specific_post(place_id, amenity_id):
-    """ post the inputed object from review objects"""
-    place = storage.get(classes['Place'], place_id)
-    amenity = storage.get(classes['Amenity'], amenity_id)
-    if not place or not amenity:
-        abort(404)
-    if storage_t == 'db':
-        check = place.amenities
-    else:
-        check = place.amenity_id
-    for task in check:
-        if task == amenity:
-            return task.to_dict(), 200
-    place.amenities.append(amenity)
-    amenity.save()
-    return amenity.to_dict(), 201
+    if request.method == 'POST':
+        place = storage.get(classes["Place"], place_id)
+        if place is None:
+            abort(404)
+        amenity = storage.get(["Amenity"], amenity_id)
+        if amenity is None:
+            abort(404)
+
+        if storage_t == "db":
+            amenities = place.amenities
+        else:
+            amenities = place.amenity_id
+        for a in amenities:
+            if (a == amenity):
+                return jsonify(amenity.to_dict()), 200
+
+        place.amenities.append(amenity)
+        amenity.save()
+        return jsonify(amenity.to_dict()), 201
